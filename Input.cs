9102555e52e422910aa043eb92df95bff5c4a68d
Cameron Thompson <tho17015@byui.edu>
2019-06-12 09:47:45 -0600

    started input interace/class?

---
+++ Input.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+
+namespace Wololo2
+{
+    class Input
+    {
+        Input()
+        {
+            
+        }
+        
+        Task<string> GetDataFromConsole(string where)
+        {
+            
+        }
+
+        Task<string> GetDataFromArgs(string where)
+        {
+
+        }
+
+        Task<string> GetDataFromServer(string where)
+        {
+
+        }
+
+        Task<string> GetDataFromHardCode(string where)
+        {
+
+        }
+
+
+    }
+}
