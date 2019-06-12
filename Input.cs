b132b774752c6cf261ceaad25a3a4754961a42e7
Cameron Thompson <tho17015@byui.edu>
2019-06-12 10:21:22 -0600

    added some methodology

--- Input.cs
+++ Input.cs
@@ -9,14 +9,27 @@ namespace Wololo2
             
         }
         
-        Task<string> GetDataFromConsole(string where)
+        static Tuple<string, string> GetDataFromConsole()
         {
+            Console.WriteLine("Please enter a course ID:"); 
+            string id = System.Console.ReadLine();
             
+            Console.WriteLine("Please enter the output method: ");
+            string output = System.Console.ReadLine();
+
+
+            return Tuple.Create(id, output);
         }
 
-        Task<string> GetDataFromArgs(string where)
+        static Tuple<string> GetDataFromArgs(string[] args)
         {
+            
+            string id = args[0];
+            
+            string output = args[1];
 
+            return Tuple.Create(id, output);
+            
         }
 
         Task<string> GetDataFromServer(string where)
