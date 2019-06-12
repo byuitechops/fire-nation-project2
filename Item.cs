3f6aab6671b6ff5bcad8856400b51b0df8d83dcb
jtschwantes <jtschwantes@gmail.com>
2019-06-12 14:10:35 -0600

    added item.cs

---
+++ Item.cs
@@ -0,0 +1,10 @@
+class Item
+        {
+            public string CourseID {get;set;}
+            public string ModName {get;set;}
+            public string Name {get;set;}
+            public string ID {get;set;}
+            public string Url {get;set;}
+            public string Type {get;set;}
+            public string Published {get;set;}
+        }
