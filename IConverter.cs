c70e03c0980f0a1672218777909b231d0c8da31a
jtschwantes <jtschwantes@gmail.com>
2019-06-12 14:06:54 -0600

    more changes, now works

--- IConverter.cs
+++ IConverter.cs
@@ -1,8 +1,11 @@
+using Newtonsoft.Json.Linq;
+
 namespace Wololo2
 {
     interface IConverter
     {
          string Convert();
+         //JObject Format();
          string GetPath();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,19 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
+using Wololo;
 
 namespace Wololo2
 {
     class Program
     {
-        class ModItem
-        {
-            public string CourseID {get;set;}
-            public string ModName {get;set;}
-            public string ModItemName {get;set;}
-            public string ModID {get;set;}
-        }
-
         static public void LoopProperties(JEnumerable<JObject> jEnum)
         {
             foreach(JObject obj in jEnum)
@@ -31,45 +24,54 @@ namespace Wololo2
             var httpGet = new HttpGet();
             string data = httpGet.GetData("https://byui.instructure.com/api/v1/courses/96/modules?include[]=items&per_page=32").Result;
 
-            byte counter = 0;
             JArray jArray = JArray.Parse(data);
 
-            var modItems = new List<ModItem>();
-            var modItem = new ModItem();
+            List<dynamic> items = Format(jArray);
+
+            var converter = new Converter();
+            converter.ObjectIn(items);
+            converter.JsonOut("json.json");
+            converter.CsvOut("csv.csv");
+            //LoopProperties(jArray.Children<JObject>());
+        }
+
+        private static List<dynamic> Format(JArray jArray)
+        {
+            var items = new List<dynamic>();
+            var item = new Item();
+
             var modName = "";
             var courseID = "96";
 
-            foreach(JObject obj in jArray.Children<JObject>())
-                foreach(JProperty prop in obj.Properties())
+            foreach (JObject obj in jArray.Children<JObject>())
+                foreach (JProperty prop in obj.Properties())
                 {
-                    if(prop.Name == "name")
-                    {
+                    if (prop.Name == "name")
                         modName = prop.Value.ToString();
-                    }
-                    if(prop.Name == "items")
-                        foreach(JObject o in prop.Value.Children<JObject>())
-                            foreach(JProperty p in o.Properties())
-                                if(p.Name == "title")
+                    if (prop.Name == "items")
+                        foreach (JObject o in prop.Value.Children<JObject>())
+                            foreach (JProperty p in o.Properties())
+                            {
+                                if (p.Name == "id")
+                                    item.ID = p.Value.ToString();
+                                else if (p.Name == "title")
+                                    item.Name = p.Value.ToString();
+                                else if (p.Name == "type")
+                                    item.Type = p.Value.ToString();
+                                else if (p.Name == "url")
+                                    item.Url = p.Value.ToString();
+                                else if (p.Name == "published")
                                 {
-                                    modItem.CourseID = courseID;
-                                    modItem.ModName = modName;
-                                    modItem.ModItemName = p.Value.ToString();
-                                    modItem.ModID = "Not Implemented";
-                                    modItems.Add(modItem);
-                                    modItem = new ModItem();
+                                    item.Published = p.Value.ToString();
+                                    item.CourseID = courseID;
+                                    item.ModName = modName;
+                                    items.Add(item);
+                                    item = new Item();
                                 }
-                    counter++;
+                            }
                 }
-            // foreach(ModItem m in modItems)
-            // {
-            //     Console.WriteLine("{");
-            //     Console.WriteLine("   CourseID = {0}", m.CourseID);
-            //     Console.WriteLine("   Module Name = {0}", m.ModName);
-            //     Console.WriteLine("   Item Name = {0}", m.ModItemName);
-            //     Console.WriteLine("   Item ID = {0}", m.ModID);
-            //     Console.WriteLine("}");
-            // }
-            //LoopProperties(jArray.Children<JObject>());
+
+            return items;
         }
     }
 }
