721cef3600af8854c8ed6c977251a0b963f13271
Cameron Thompson <39208507+cdthomp1@users.noreply.github.com>
2019-06-14 13:20:22 -0600

    Update Html.cs

--- Html.cs
+++ Html.cs
@@ -1,24 +1,95 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Wololo;
+
 namespace Wololo2
 {
-    class HtmlClass : IConverter 
+    class Program
     {
-        string data;
-        readonly string path; 
-
-        HtmlClass()
+        static public void LoopProperties(JEnumerable<JObject> jEnum)
         {
-            path = "html.html";
+            foreach(JObject obj in jEnum)
+                foreach(JProperty prop in obj.Properties())
+                {
+                    if(prop.Name == "title")
+                        Console.WriteLine(prop.Value);
+                    else if(prop.Name == "items")
+                        LoopProperties(prop.Value.Children<JObject>());
+                }
         }
 
-        public string Convert()
+        static void Main(string[] args)
         {
-            string str = "";
-            return str;
+            var httpGet = new HttpGet();
+            string data = httpGet.GetData("https://byui.instructure.com/api/v1/courses/96/modules?include[]=items&per_page=32").Result;
+
+            JArray jArray = JArray.Parse(data);
+
+            JArray items = Format(jArray);
+
+            var converter = new Converter();
+            converter.jArray = items;
+            converter.JsonOut("json.json");
+            converter.CsvOut("csv.csv");
+            Html html = new Html();
+            html.Convert(items);
+            //LoopProperties(jArray.Children<JObject>());
         }
 
-        public string GetPath()
+        private static JArray Format(JArray jArray)
         {
-            return path;
+            var item = new Item();
+
+            var modName = "";
+            var courseID = "96";
+
+            JArray myModArr = new JArray();
+            JObject myModObj = new JObject();
+            JArray myItemArr = new JArray();
+
+            foreach (JObject obj in jArray.Children<JObject>())
+            {
+                //Console.WriteLine((string)obj.SelectToken("name"));
+                modName = (string)obj.SelectToken("name");
+                foreach (JObject o in obj.SelectToken("items").Children<JObject>())
+                {
+                    item.CourseID = courseID;
+                    item.ModName = modName;
+                    item.ID = o.SelectToken("id").ToString();
+                    item.Name = o.SelectToken("title").ToString();
+                    item.Type = o.SelectToken("type").ToString();
+                    try
+                    {
+                        item.Url = o.SelectToken("html_url").ToString();
+                    }
+                    catch (Exception e)
+                    {
+                        try{item.Url = o.SelectToken("html_url").ToString();}
+                        catch(Exception e2){item.Url = "#";}
+                    }
+                    item.Published = o.SelectToken("published").ToString();
+                    myItemArr.Add(JObject.FromObject(item));
+                    item = new Item();
+                }
+                
+                myModObj.Add(new JProperty("name", obj.SelectToken("name")));
+                myModObj.Add(new JProperty("items", myItemArr));
+                myModArr.Add(myModObj);
+                //Console.WriteLine(myModArr);
+
+                myModObj = new JObject();
+                myItemArr = new JArray();
+            }
+            
+            JArray courseArr = new JArray();
+            JObject courseObj = new JObject();
+
+            courseObj.Add(new JProperty("name", courseID));
+            courseObj.Add(new JProperty("modules", myModArr));
+            courseArr.Add(courseObj);
+            
+            return courseArr;
         }
     }
 }
