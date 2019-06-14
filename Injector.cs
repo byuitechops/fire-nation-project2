27e3849e9b3c9b6be517800319e8888ae1562b16
Wyndham <mattwyndham@byui.edu>
2019-06-14 15:31:06 -0600

    static injector

--- Injector.cs
+++ Injector.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
 
 namespace Wololo2
 {
     class Injector
     {
-        
+        static internal List<dynamic> Startup(Tuple<string, string> input)
+        {
+            
+        }
     }
 }
